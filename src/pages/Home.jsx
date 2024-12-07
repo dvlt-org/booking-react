33dcc9734cab602230eaf3ecee31068a72bdb8bd
dvltinv <dvltinv@gmail.com>
2024-12-07 11:45:12 +0500

    featured-property

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar"
 import Header from "../components/Header"
 import Featured from "../components/Featured"
 import PropertyList from "../components/PropertyList"
+import FeaturedProperty from "../components/FeaturedProperty"
 import "./pages.css"
 
 export default function Home() {
@@ -11,10 +12,12 @@ export default function Home() {
             <Navbar />
             <Header />
             <div className="home-container">
-                <h1>Featured</h1>
+                <h1 className='title'>Featured</h1>
                 <Featured />
-                <h1 style={{ width: "100%", maxWidth: "1024px", fontSize: "20px" }}>Browse by property type</h1>
+                <h1 className='title'>Browse by property type</h1>
                 <PropertyList />
+                <h1 className='title'>Homes guests love</h1>
+                <FeaturedProperty />
             </div>
         </div>
     )
